d431291201bc3eb5919ab226aa47002a9147fd12
JoseGu1llardi <junior11_junior@hotmail.com>
2025-11-11 20:28:32 +0000

    refactor(User): improved validation and value object integration for CPF and Email

--- src/model/entity/User.java
+++ src/model/entity/User.java
@@ -1,6 +1,7 @@
 package model.entity;
 
 import model.valueObject.CPF;
+import model.valueObject.Email;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -9,12 +10,41 @@ public class User {
     private final String id;
     private String name;
     private CPF cpf;
-    private String email;
+    private Email email;
     private LocalDateTime createdAt;
 
     public User(String name, String cpf, String email) {
         this.id = UUID.randomUUID().toString();
         this.name = name;
         this.cpf = new CPF(cpf);
+        this.email = Email.of(email);
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public void setName(String name) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException("Name cannot be null or empty.");
+        }
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public CPF getCpf() {
+        return cpf;
+    }
+
+    public Email getEmail() {
+        return email;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
     }
 }
