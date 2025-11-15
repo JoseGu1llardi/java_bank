e0a72f2d5ab65b4305735c2fd7f7d889e531ddaa
JoseGu1llardi <junior11_junior@hotmail.com>
2025-11-15 22:31:03 +0000

    Add Transaction class with authentication code generation and transaction details.

---
+++ src/model/entity/Transaction.java
@@ -0,0 +1,45 @@
+package model.entity;
+
+import model.enums.TransactionType;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.UUID;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Transaction {
+    private final String id;
+    private final TransactionType type;
+    private final BigDecimal amount;
+    private final BigDecimal previousBalance;
+    private final BigDecimal balanceAfter;
+    private final LocalDateTime dateTime;
+    private final String originAccount;
+    private final String destinationAccount;
+    private final String description;
+    private String status;
+    private final String authenticationCode;
+
+    public Transaction(TransactionType type, BigDecimal amount, BigDecimal previousBalance,
+                       BigDecimal balanceAfter, String originAccount,
+                       String destinationAccount, String description) {
+        this.id = UUID.randomUUID().toString();
+        this.type = type;
+        this.amount = amount;
+        this.previousBalance = previousBalance;
+        this.balanceAfter = balanceAfter;
+        this.dateTime = LocalDateTime.now();
+        this.originAccount = originAccount;
+        this.destinationAccount = destinationAccount;
+        this.description = description;
+        this.authenticationCode = generateAuthenticationCode();
+
+    }
+
+    private String generateAuthenticationCode() {
+        return String.format("%s-%06d",
+                LocalDateTime.now().format(DateTimeFormatter.BASIC_ISO_DATE),
+                ThreadLocalRandom.current().nextInt(100000, 1_000_000));
+    }
+}
