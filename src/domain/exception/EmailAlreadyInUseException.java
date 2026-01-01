c7517e11dec0ac7f46551e521901ed4eafd31037
JoseGu1llardi <2024521@student.cct.ie>
2026-01-01 17:56:48 +0000

    feat(exception): add EmailAlreadyInUseException runtime exception
                                    Adds custom exception to represent email duplication when attempting to
                                    create a user or persist an account with an already registered email..

---
+++ src/domain/exception/EmailAlreadyInUseException.java
@@ -0,0 +1,7 @@
+package domain.exception;
+
+public class EmailAlreadyInUseException extends RuntimeException {
+    public EmailAlreadyInUseException(String message) {
+        super(message);
+    }
+}
