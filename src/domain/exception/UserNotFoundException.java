43345408e6ccafc74505386505bb76c6349f2e74
JoseGu1llardi <2024521@student.cct.ie>
2026-01-01 18:05:44 +0000

    refactor(exception): simplify UserNotFoundException and remove userId context
                   Replaces parameterized constructor and detailed message with a generic
                   'User not found.' text, making exception reusable and less coupled to ID
                   semantics.

--- src/domain/exception/UserNotFoundException.java
+++ src/domain/exception/UserNotFoundException.java
@@ -1,7 +1,7 @@
 package domain.exception;
 
 public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String userId) {
-        super("Could not find user with id: " + userId);
+    public UserNotFoundException() {
+        super("User not found.");
     }
 }
