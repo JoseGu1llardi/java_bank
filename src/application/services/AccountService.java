c86fea17168a0558d96908a95dcf1eef4e7e356f
JoseGu1llardi <junior11_junior@hotmail.com>
2026-01-02 19:52:53 +0000

    feat(AccountService): add account lookup, search, and deactivation use cases - getAccount to retrieve account by agency and number - searchForUserAccounts to list all accounts owned by a user - deactivateAccount with business rules preventing deactivation when balance is non-zero

--- src/application/services/AccountService.java
+++ src/application/services/AccountService.java
@@ -6,7 +6,11 @@ import domain.entity.Account;
 import domain.entity.CheckingAccount;
 import domain.entity.SavingsAccount;
 import domain.entity.User;
+import domain.exception.AccountNotFoundException;
 import domain.exception.UserNotFoundException;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 public class AccountService {
     private final AccountRepository accountRepository;
@@ -31,5 +35,27 @@ public class AccountService {
         Account newAccount =  new SavingsAccount(agency, holder);
         accountRepository.save(newAccount);
         return newAccount;
+    }
+
+    public Account getAccount(String agency, String number) {
+        return accountRepository.getByKey(agency, number).orElseThrow(AccountNotFoundException::new);
+    }
+
+    public List<Account> searchForUserAccounts(String userId) {
+        return accountRepository.getByHolder(userId).stream().toList();
+    }
+
+    public void deactivateAccount(String agency, String number) {
+        Account account = accountRepository.getByKey(agency, number).orElseThrow(AccountNotFoundException::new);
+
+        if (account.getBalance().compareTo(BigDecimal.ZERO) > 0) {
+            throw new IllegalArgumentException("It is not possible to deactivate account with balance.");
+        } else if (account.getBalance().compareTo(BigDecimal.ZERO) < 0) {
+            throw new  IllegalArgumentException("It is not possible to deactivate account with negative balance, " +
+                    "you must pay first.");
+        }
+
+        account.disable();
+        accountRepository.save(account);
     }
 }
