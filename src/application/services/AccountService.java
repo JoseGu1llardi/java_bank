39b7e7f82cd735c4c457739b8d99e67e50f48d23
JoseGu1llardi <junior11_junior@hotmail.com>
2026-01-02 19:13:18 +0000

    feat(AccountService): add createSavingAccount use case
                   Validates account holder existence and persists the new account via AccountRepository.

--- src/application/services/AccountService.java
+++ src/application/services/AccountService.java
@@ -4,12 +4,13 @@ import application.repositories.AccountRepository;
 import application.repositories.UserRepository;
 import domain.entity.Account;
 import domain.entity.CheckingAccount;
+import domain.entity.SavingsAccount;
 import domain.entity.User;
 import domain.exception.UserNotFoundException;
 
 public class AccountService {
-    private AccountRepository accountRepository;
-    private UserRepository userRepository;
+    private final AccountRepository accountRepository;
+    private final UserRepository userRepository;
 
     public AccountService(AccountRepository accountRepository, UserRepository userRepository) {
         this.accountRepository = accountRepository;
@@ -20,6 +21,14 @@ public class AccountService {
         User holder = userRepository.findById(userId).orElseThrow(UserNotFoundException::new);
 
         Account newAccount = new CheckingAccount(agency, holder);
+        accountRepository.save(newAccount);
+        return newAccount;
+    }
+
+    public Account createSavingAccount(String agency, String userId) {
+        User holder = userRepository.findById(userId).orElseThrow(UserNotFoundException::new);
+
+        Account newAccount =  new SavingsAccount(agency, holder);
         accountRepository.save(newAccount);
         return newAccount;
     }
